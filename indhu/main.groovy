36ca70764a379cf1c85bdd100343186b2154be30
Natsai, Sobitha (ESI) <sobitha.natsai@adp.com>
2019-08-11 22:23:19 +0530

    fix

--- indhu/main.groovy
+++ indhu/main.groovy
@@ -1,20 +1,17 @@
 import groovy.json.JsonSlurper
 import org.apache.commons.codec.binary.Base64;
 
-			String webPage = "http://192.168.43.238:7990/rest/api/1.0/projects/HALO/repos";
-			String name = "shever";
-			String password = "redhat";
-
-			String authString = name + ":" + password;
-			System.out.println("auth string: " + authString);
-			byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
-			String authStringEnc = new String(authEncBytes);
-			System.out.println("Base64 encoded auth string: " + authStringEnc);
-
-			URL url = new URL(webPage);
-			URLConnection urlConnection = url.openConnection();
-			urlConnection.setRequestProperty("Authorization", "Basic " + authStringEnc);
-//            println (urlConnection.getInputStream())
+String webPage = "http://192.168.43.238:7990/rest/api/1.0/projects/HALO/repos";
+String name = "shever";
+String password = "redhat";
+String authString = name + ":" + password;
+System.out.println("auth string: " + authString);
+byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
+String authStringEnc = new String(authEncBytes);
+System.out.println("Base64 encoded auth string: " + authStringEnc);
+URL url = new URL(webPage);
+URLConnection urlConnection = url.openConnection();
+urlConnection.setRequestProperty("Authorization", "Basic " + authStringEnc);
 
 def output = urlConnection.getInputStream().getText();
 
@@ -29,6 +26,21 @@ repo_names.add('"' + repo.name + '"')
 println (repo_names)
 
 
+def parser = new groovy.json.JsonSlurper()
+def path = jobFactory.readFileFromWorkspace('indhu/env.json')
+env_data = parser.parseText(path)
+env_list = env_list.keySet() 
+devEnvList = []
+
+// for (data in env_data) {
+// def matcher = data.key =~ /DEV\d/
+// if ( matcher.find()){
+//     devEnvList.add(data)
+// }
+
+// }
+
+
 def CreatePipelineJob() {
     pipelineJob("shever-testing") {
         logRotator {
@@ -36,6 +48,8 @@ def CreatePipelineJob() {
         }
     environmentVariables {
             envs('repo_list': "${repo_names}")
+            envs('env_data': "${env_data}")
+            envs('env_list': "${env_list}")
         }
 
         //displayName("#${BUILD_NUMBER} ${ENV}")
@@ -48,4 +62,5 @@ def CreatePipelineJob() {
                 }
 
 }
+
 CreatePipelineJob()
